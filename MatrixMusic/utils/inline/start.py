fada3794988568f987bb669fb1e9d8b6bbb1a529
hossamhosss <153024340+hossamhosss@users.noreply.github.com>
2024-03-28 14:17:12 +0200

    Add files via upload

---
+++ MatrixMusic/utils/inline/start.py
@@ -0,0 +1,36 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from MatrixMusic import app
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text="𝙶𝚁𝙾̀𝚄𝙿", url= "https://t.me/teamtitoooooo"),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        
+        [
+            InlineKeyboardButton(text="مطور البوت", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="𝙶𝚁𝙾̀𝚄𝙿", url=f"https://t.me/teamtitoooooo"), 
+        ],
+        [
+            
+            InlineKeyboardButton(text="𝑺𝑶𝑼𝑹𝑪𝑬 𝑯𝑶𝑺𝑺𝑨𝑴", url=f"https://t.me/UU_GR") , 
+        ],
+    ]
+    return buttons
