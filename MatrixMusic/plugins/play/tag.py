ac13446c97435a0f198a581b0eedc1d8641e1f55
hossamhosss <153024340+hossamhosss@users.noreply.github.com>
2024-04-03 03:48:45 +0200

    Add files via upload

--- MatrixMusic/plugins/play/tag.py
+++ MatrixMusic/plugins/play/tag.py
@@ -1,9 +1,9 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/TeamYukki/MatrixMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/TeamYukki/MatrixMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 import re
@@ -15,10 +15,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils import bot_sys_stats
-from YukkiMusic.utils.decorators.language import language
+from MatrixMusic import app
+from MatrixMusic.core.call import Yukki
+from MatrixMusic.utils import bot_sys_stats
+from MatrixMusic.utils.decorators.language import language
 import asyncstdlib as a
 import asyncio
 
