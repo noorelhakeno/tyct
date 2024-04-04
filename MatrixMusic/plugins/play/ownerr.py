27c16442f316558e37cb7e7917a27511a092dbc8
hossamhosss <153024340+hossamhosss@users.noreply.github.com>
2024-04-04 14:56:41 +0200

    Add files via upload

--- MatrixMusic/plugins/play/ownerr.py
+++ MatrixMusic/plugins/play/ownerr.py
@@ -53,7 +53,7 @@ async def khfzss(client: Client, message: Message):
     id = usrr.id
     username = usrr.username
     async for photo in client.get_chat_photos(OWNER_ID, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""- 𓏺 َِՏΌႮᎡᏟᎬ ᏞΌͲႮՏ : \n\n⌔︙𝒏𝒂𝒎𝒆: {name} \n\n⌔︙𝒖𝒔𝒆𝒓: @{username} \n\n⌔︙𝒊𝒅: {id} \n\n⌔︙𝒃𝒊𝒐: {bio} \n\n⌔︙𝒋𝒐𝒃:  مطور البوت """, 
+                    await message.reply_photo(photo.file_id,       caption=f"""- 𓏺 َِ𝑺𝑶𝑼𝑹𝑪𝑬 𝑯𝑶𝑺𝑺𝑨𝑴: \n\n⌔︙𝒏𝒂𝒎𝒆: {name} \n\n⌔︙𝒖𝒔𝒆𝒓: @{username} \n\n⌔︙𝒊𝒅: {id} \n\n⌔︙𝒃𝒊𝒐: {bio} \n\n⌔︙𝒋𝒐𝒃:  مطور البوت """, 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
