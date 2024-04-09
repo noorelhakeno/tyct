1f4f7b668c6a8ff0641799e71b4b945629250219
hossamhosss <153024340+hossamhosss@users.noreply.github.com>
2024-04-09 03:11:03 +0200

    Add files via upload

---
+++ MatrixMusic/plugins/play/cmds.py
@@ -0,0 +1,43 @@
+import asyncio
+import os
+import requests
+import pyrogram
+from pyrogram import Client, filters, emoji
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram.errors import MessageNotModified
+from MatrixMusic import app
+from config import OWNER_ID, LOGGER_ID
+
+
+@app.on_message(command(["الاوامر"]))
+async def zdatsr(client: Client, message: Message):
+    usr = await client.get_users(OWNER_ID)
+    name = usr.first_name
+    usrnam = usr.username
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/a25271d31dd6e1bca849f.jpg",
+        caption=f"""<b>↯︙مرحباً بك عزيزي</b>\n<b>↯︙استخدم الازرار بالاسفل\n» ل تصفح اوامر الميوزك</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "‹ اوامر التشغيل ›", callback_data="zzzll"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ اوامر القناة ›", callback_data="zzzch"),
+                    InlineKeyboardButton(
+                        "‹ اوامر الادمن ›", callback_data="zzzad"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ اوامر المطور ›", callback_data="zzzdv"),
+                ],[
+                    InlineKeyboardButton(name, url=f"https://t.me/{usrnam}"),
+                ],[
+                    InlineKeyboardButton(
+                        "𝑺𝑶𝑼𝑹𝑪𝑬 𝑯𝑶𝑺𝑺𝑨𝑴", url="https://t.me/UU_GR"),
+                   ],
+            ]
+        ),
+    )     
+         
