6230470d47388affedf235d00fdfc18b83b0116d
nornorno <noureteo@gmail.com>
2024-06-08 02:00:42 +0300

    Update CR.py

--- MatrixMusic/plugins/play/CR.py
+++ MatrixMusic/plugins/play/CR.py
@@ -54,7 +54,7 @@ async def huhh(client: Client, message: Message):
 
 
 @app.on_message(
-    command([""مطور السورس", "مبرمج السورس", "المطور""])
+    command(["مطور السورس","مبرمج السورس","المطور"])
     
     
 )
@@ -75,7 +75,7 @@ async def yas(client, message):
 
 
 @app.on_message(
-    command([""مطور السورس", "مبرمج السورس", "نور""])
+    command(["مطور السورس","مبرمج السورس","نور"])
     
     
 )
