8aeb005334016c0fa0b9d97f30ca003c86dfc985
nornorno <noureteo@gmail.com>
2024-06-07 23:44:27 +0300

    Update devwelcome.py

--- MatrixMusic/plugins/play/devwelcome.py
+++ MatrixMusic/plugins/play/devwelcome.py
@@ -6,7 +6,7 @@ import os
 
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member, group=847)
 async def WelcomeDev(_, response: ChatMemberUpdated):
-    dev_id = 5554836675 # حط ايديك هنا
+    dev_id = 6799500549 # حط ايديك هنا
     if response.from_user.id == dev_id:
         info = await app.get_chat(dev_id)
         name = info.first_name
@@ -19,6 +19,6 @@ async def WelcomeDev(_, response: ChatMemberUpdated):
             chat_id=response.chat.id,
             reply_markup=markup,
             photo="downloads/developer.jpg", 
-            caption=f"-  مطور سورس حسام⚡ {name} دخل الشات.\n- {bio}"
+            caption=f"- Whoever humbles #himself to god will be #exalted 𓏺 {name} دخل الشات.\n- {bio}"
         )
     
