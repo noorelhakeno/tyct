0795245fc7fce3a1c05bcaa3ff79e21ef36b1fbe
nornorno <noureteo@gmail.com>
2024-06-08 03:57:46 +0300

    Update CR.py

--- MatrixMusic/plugins/play/CR.py
+++ MatrixMusic/plugins/play/CR.py
@@ -61,7 +61,7 @@ async def huhh(client: Client, message: Message):
     
 )
 async def yas(client, message):
-    usr = await client.get_chat("NOR_O")
+    usr = await client.get_chat("TTlTITl")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"معلومات مطور السورس\n\n‍ ¦dev :{name}\n\n ¦user :@{usr.username}\n\n ¦id :`{usr.id}`\n\n ¦bio :{usr.bio}\n\nســورس ميــوزك العـالم", 
@@ -82,7 +82,7 @@ async def yas(client, message):
     
 )
 async def yas(client, message):
-    usr = await client.get_chat("NOR_O")
+    usr = await client.get_chat("TTlTITl")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"معلومات مطور السورس.\n\n¦dev :{name}\n\n ¦user :@{usr.username}\n\n ¦id :`{usr.id}`\n\n ¦boi :{usr.bio}\n\nسـورس مـيوزك العـالم", 
