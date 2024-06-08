13ff9ef9954c349bc51ed8af2bcbab61e5355175
nornorno <noureteo@gmail.com>
2024-06-08 20:50:42 +0300

    Update __main__.py

--- MatrixMusic/__main__.py
+++ MatrixMusic/__main__.py
@@ -36,13 +36,13 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("MatrixMusic.plugins" + all_module)
-    LOGGER("ميوزك حسام").info("تم تحميل الاضافات ...✓")
+    LOGGER("ميوزك العالم").info("تم تحميل الاضافات ...✓")
     await userbot.start()
     await Zelzaly.start()
     try:
         await Zelzaly.stream_call("https://telegra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("ميوزك حسام").info(
+        LOGGER("ميوزك العالم").info(
             "خطأ .. قم بفتح المكالمة في مجموعة السجل الخاصه بك\n\nجارِ ايقاف بوت الميوزك . . ."
         )
         exit()
@@ -53,7 +53,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("ميوزك حسام").info("جارِ ايقاف بوت الميوزك . . .")
+    LOGGER("ميوزك العالم").info("جارِ ايقاف بوت الميوزك . . .")
 
 
 if __name__ == "__main__":
