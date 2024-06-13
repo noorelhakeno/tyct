3f92a7d4b59c4f1265196bf3cd8e4770d9ad9aef
noorelhakeno <152221032+noorelhakeno@users.noreply.github.com>
2024-06-13 14:03:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nornorno/mouozk-2024",
+    "https://github.com/noooooorrrrf/tyct",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
